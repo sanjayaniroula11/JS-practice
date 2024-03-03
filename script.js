2d1a4c627fa28081b9518d3fce6d7b2b3ac18ca1
Sanjaya Niroula <niroulasanjaya42@gmail.com>
2024-03-04 01:02:46 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// let myName= "sanjaya", myNickname="Niroula";
+// myName= myNickname;
+// alert(myNickname);
+
+//Naming the variables right
+
+//1 
+let ourPlanetName= "Earth";
+//2
+let currentUserName= "name of the person visiting the website right now"
+
+//-----------------------
+
+// let myString="10";
+// isNaN(myString);
+
+// let apple = "4";
+// let mango = "5";
+// alert(apple+mango);
+// alert(+apple + +mango);
+
+// let counter = 0;
+// ++counter;
+// counter++;
+
+
+//  let counter = 0;
+// function counterLearn() {
+//     counter++;
+//     alert(counter);
+
+// }
+// counterLearn()
+// alert(counter);
+
+const clickable = document.querySelector("button");
+
+function clickToSee() { 
+    const name = prompt('what is your name?');
+    const message = document.querySelector("#paragraph");
+    message.innerHTML=`Hello, ${name} nice to meet you`;
+}
+
+clickable.addEventListener('click', clickToSee);
